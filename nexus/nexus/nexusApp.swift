b3ab16124d1b99285e46975317cdaabb02e771b3
DanielTantsyura <dvt@andrew.cmu.edu>
2025-03-29 00:39:48 -0400

    created xcode app template

---
+++ nexus/nexus/nexusApp.swift
@@ -0,0 +1,17 @@
+//
+//  nexusApp.swift
+//  nexus
+//
+//  Created by daniel tantsyura on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct nexusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nexus/nexusTests/nexusTests.swift
@@ -0,0 +1,17 @@
+//
+//  nexusTests.swift
+//  nexusTests
+//
+//  Created by daniel tantsyura on 3/29/25.
+//
+
+import Testing
+@testable import nexus
+
+struct nexusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
